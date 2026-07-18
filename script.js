de5b8372e51b2b03cc7d01eadfb156a64a560038
adityaakash31-pixel <adityaakash31@gmail.com>
2026-07-18 08:46:10 +0530

    Implement income saving functionality
    Added functionality to save income with validation for category and amount.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// ===============================
+// ArthaFlow V1.0
+// Step 9
+// ===============================
 
+// Total Income
+let totalIncome = 0;
+
+// Save Income
+function saveIncome() {
+
+    let category = document.getElementById("category").value;
+    let amount = Number(document.getElementById("amount").value);
+
+    if (category === "") {
+        alert("Please enter category");
+        return;
+    }
+
+    if (amount <= 0) {
+        alert("Please enter valid amount");
+        return;
+    }
+
+    totalIncome = totalIncome + amount;
+
+    alert("Income Saved Successfully");
+
+    document.getElementById("category").value = "";
+    document.getElementById("amount").value = "";
+
+}
