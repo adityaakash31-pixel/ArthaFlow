1d59fa851283c8369a0bcbf5f4adbce6e3870563
adityaakash31-pixel <adityaakash31@gmail.com>
2026-08-03 00:38:21 +0530

    Refactor monthly history rendering in script.js
    Refactored monthly history rendering logic to reduce redundancy and improve clarity.

--- js/script.js
+++ js/script.js
@@ -1412,46 +1412,25 @@ document.getElementById("monthlyHistory");
 
 if(monthlyHistory){
 
-let balance =
-totalIncome-totalExpense;
+    monthlyHistory.innerHTML = "";
 
-let monthlyHistory =
-document.getElementById("monthlyHistory");
+    let data =
+    JSON.parse(localStorage.getItem("monthlyData")) || {};
 
-if(monthlyHistory){
+    for(let month in data){
 
-let balance =
-totalIncome-totalExpense;
+        monthlyHistory.innerHTML +=
+        "<tr>" +
+        "<td>"+month+"</td>" +
+        "<td>₹"+data[month].income+"</td>" +
+        "<td>₹"+data[month].expense+"</td>" +
+        "<td>₹"+data[month].balance+"</td>" +
+        "</tr>";
 
-let monthlyHistory =
-document.getElementById("monthlyHistory");
-
-if(monthlyHistory){
-
-monthlyHistory.innerHTML="";
-
-let data =
-JSON.parse(localStorage.getItem("monthlyData")) || {};
-
-for(let month in data){
-
-monthlyHistory.innerHTML +=
-
-"<tr>" +
-
-"<td>"+month+"</td>"+
-
-"<td>₹"+data[month].income+"</td>"+
-
-"<td>₹"+data[month].expense+"</td>"+
-
-"<td>₹"+data[month].balance+"</td>"+
-
-"</tr>";
+    }
 
 }
 
-}
 
 // ===============================
 // Monthly Record Save
