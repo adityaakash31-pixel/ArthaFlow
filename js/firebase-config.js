2e581a5869d3e76e31d04aa9e9146400738e96fd
adityaakash31-pixel <adityaakash31@gmail.com>
2026-08-07 20:28:57 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,6 @@
-// ======================================
-// ArthaFlow - Firebase Configuration
-// ======================================
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.1/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDM8WtiKL5hSSoDH4BoacMUIQ3QVSpjxJI",
@@ -12,12 +8,12 @@ const firebaseConfig = {
     projectId: "arthaflow-4d670",
     storageBucket: "arthaflow-4d670.firebasestorage.app",
     messagingSenderId: "417281404304",
-    appId: "1:417281404304:web:a0b9fa5a787ad0426d5715"
+    appId: "1:417281404304:web:a0b9fa5a787ad0426d5715",
+    measurementId: "G-785TXW9WTL"
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-export { app, auth, db };
+export { auth };
